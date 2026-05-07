c1f1f5f7f2fd60034fcd8a7cd31d68c2f15dbe98
veil-dev <velz0952@gmail.com>
2026-05-07 17:41:40 +0800

    remove node_modules from tracking

--- node_modules/xrpl/dist/npm/Wallet/authorizeChannel.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Wallet } from './index';
-export declare function authorizeChannel(wallet: Wallet, channelId: string, amount: string): string;
-//# sourceMappingURL=authorizeChannel.d.ts.map
